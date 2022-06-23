ea18241506f7676a0d54dfc1b0b3605f30bb5fd3
Mihail <Dante9041@mail.ru>
2022-06-23 15:57:50 +0300

    add DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = Input("A: ");
+int b = Input("B: ");
+int result=1;
+for(int i = 0; i< b;i++)
+{
+    result *=a ;
+
+}
+Console.WriteLine("A^B="+result);
+
+
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
