17578a9b4bed6f66b6dc0d2dcb006bcf054fa625
Mihail <Dante9041@mail.ru>
2022-06-23 16:00:28 +0300

    add DZ2

---
+++ DZ2/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int nu = new Random().Next(100,1000);
+Console.WriteLine(nu);
+int index = 1;
+int result = 0;
+while(nu > 0)
+{
+    int n = nu % 10;
+    result+=n;
+    index ++; 
+    nu = nu / 10;
+}
+Console.WriteLine($"Ответ {result}"); 
