813cf9cc9521c5124fad61859e68f145579a5fbc
Mihail <Dante9041@mail.ru>
2022-06-23 16:00:55 +0300

    add DZ3

---
+++ DZ3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] arr = new int[8]; 
+
+for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+
+Vivodmasiv(arr);
+
+void Vivodmasiv(int[] arr)
+{
+    int size=arr.Length;
+    Console.WriteLine(size);
+    Console.Write($"[");
+    for(int i = 0; i < size-1; i++)
+    {
+        Console.Write($"{arr[i]},");
+    }
+    Console.Write($"{arr[size-1]}]");
+    Console.WriteLine();
+}
+
